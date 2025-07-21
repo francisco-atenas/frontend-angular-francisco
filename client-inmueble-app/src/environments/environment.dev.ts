53e4877afd63fb6f616081930aadaad52d36ed16
francisco-atenas <franciscoorozco@atenasconsultores.com>
2025-07-21 16:48:42 -0400

    terminada lección 71

--- client-inmueble-app/src/environments/environment.dev.ts
+++ client-inmueble-app/src/environments/environment.dev.ts
@@ -1,4 +1,16 @@
+import { config } from 'rxjs';
+
 export const environment = {
   production: false,
   name: 'dev',
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyCuPmq0OK_0lyhEpbtJrPiDauSMtz1XzY0',
+      authDomain: 'edificacion-app-613c8.firebaseapp.com',
+      projectId: 'edificacion-app-613c8',
+      storageBucket: 'edificacion-app-613c8.firebasestorage.app',
+      messagingSenderId: '423490016847',
+      appId: '1:423490016847:web:fbeb32ded6ab3f01937c3a',
+    },
+  },
 };
--- client-inmueble-app/src/environments/environment.prod.ts
+++ client-inmueble-app/src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
   name: 'prod',
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyCuPmq0OK_0lyhEpbtJrPiDauSMtz1XzY0',
+      authDomain: 'edificacion-app-613c8.firebaseapp.com',
+      projectId: 'edificacion-app-613c8',
+      storageBucket: 'edificacion-app-613c8.firebasestorage.app',
+      messagingSenderId: '423490016847',
+      appId: '1:423490016847:web:fbeb32ded6ab3f01937c3a',
+    },
+  },
 };
