40af0c741c171efb7a451ed44562d2e8bb670561
francisco-atenas <franciscoorozco@atenasconsultores.com>
2025-07-22 10:36:29 -0400

    avance leccion 72

--- client-inmueble-app/src/app/app.component.ts
+++ client-inmueble-app/src/app/app.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
-import { environment } from '@src/environments/environment';
-import { test } from '@app/test';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cli-inmueble-app';
+
+  constructor(private fs: AngularFirestore) {
+    // Example of using AngularFirestore
+    // this.fs.collection('test').valueChanges().subscribe(data => {
+    //   console.log('Data from Firestore:', data);
+    // });
+  }
+  ngOnInit(): void {
+    // Fetch initial data from Firestore on component initialization
+    this.fs
+      .collection('test')
+      .stateChanges()
+      .subscribe((personas) => {
+        console.log(
+          'Initial data from Firestore (ngOnInit):',
+          personas.map((p) => p.payload.doc.data())
+        );
+      });
+  }
 }
