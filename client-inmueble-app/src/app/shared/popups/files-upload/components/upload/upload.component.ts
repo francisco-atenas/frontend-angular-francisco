cc0657b4faaf2fe55a2ee78ea27632a712f266e8
francisco-atenas <franciscoorozco@atenasconsultores.com>
2025-07-29 11:59:47 -0400

    Añadir componente de carga de archivos

---
+++ client-inmueble-app/src/app/shared/popups/files-upload/components/upload/upload.component.ts
@@ -0,0 +1,71 @@
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  Output,
+} from '@angular/core';
+import {
+  AngularFireStorage,
+  AngularFireUploadTask,
+} from '@angular/fire/compat/storage';
+import { UploadTaskSnapshot } from '@angular/fire/compat/storage/interfaces';
+import { Observable, Subject, lastValueFrom } from 'rxjs';
+import { takeUntil, finalize } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-upload',
+  templateUrl: './upload.component.html',
+  styleUrls: ['./upload.component.scss'],
+})
+export class UploadComponent implements OnInit, OnDestroy {
+  @Input() file!: File;
+  @Output() completed = new EventEmitter<string>();
+
+  task!: AngularFireUploadTask;
+
+  snapshot$!: Observable<UploadTaskSnapshot | undefined>;
+
+  percentage$!: Observable<number | undefined>;
+
+  private destroy$ = new Subject<void>();
+
+  downloadURL!: string;
+
+  constructor(private storage: AngularFireStorage) {}
+
+  ngOnInit(): void {}
+
+  startUploading(): void {
+    const path = `${this.file.type.split('/')[0]}/${Date.now()}_${
+      this.file.name
+    }`;
+
+    const storageRef = this.storage.ref(path);
+
+    this.task = this.storage.upload(path, this.file);
+
+    this.percentage$ = this.task.percentageChanges();
+
+    this.snapshot$ = this.task.snapshotChanges() as Observable<
+      UploadTaskSnapshot | undefined
+    >;
+
+    this.snapshot$
+      .pipe(
+        takeUntil(this.destroy$),
+        finalize(async () => {
+          const storageRefObservable$ = storageRef.getDownloadURL();
+          this.downloadURL = await lastValueFrom(storageRefObservable$);
+          this.completed.next(this.downloadURL);
+        })
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
